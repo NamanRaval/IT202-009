13a34d5f499a4f26b845b40e4d4cc1f3e0cc88a8
NamanRaval <nyr2@njit.edu>
2021-09-22 19:29:57 -0400

    iterated through array and added each value to total

--- public_html/HW2/Problem2.php
+++ public_html/HW2/Problem2.php
@@ -7,7 +7,14 @@ function getTotal($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     $total = 0.00;
     //TODO do adding here
+    foreach($arr as $value){
+        $total = $value + $total;
+
+    }
     //TODO do rounding stuff here
+    
+
+
     echo "The total is " . var_export($total, true);
 }
 echo "Problem 2: Adding Floats<br>";
