00d0905a80ef13fec96538e5f7d0e0068914baf3
NamanRaval <nyr2@njit.edu>
2021-10-14 13:44:27 -0400

    Done

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,16 +1,14 @@
 <?php
-require(__DIR__."/../../partials/nav.php");
+require(__DIR__ . "/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
-if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
- flash("Welcome, " . $_SESSION["user"]["email"]); 
-}
-else{
-  flash("You're not logged in");
-}
+if (is_logged_in(true)) {
+    echo "Welcome home, " . get_username();
+    //comment this out if you don't want to see the session variables
+    echo "<pre>" . var_export($_SESSION, true) . "</pre>";
+} 
 ?>
-
 <?php
-require(__DIR__."/../../partials/flash.php”);
+require(__DIR__ . "/../../partials/flash.php");
 ?>
