1c5a91ae36b86160c97926f3768fe8e1abb4ac99
NamanRaval <nyr2@njit.edu>
2021-11-24 17:14:04 -0500

    different types of accounts

--- public_html/Project/test_create_accounts.php
+++ public_html/Project/test_create_accounts.php
@@ -5,3 +5,17 @@ if (!has_role("Admin")) {
   die(header("Location: login.php"));
 }
 ?> 
+
+<form method="POST">
+	<label>Account Number</label>
+	<input name="account_number" type="number" max="999999999999" min="100000000000"/> 
+	<label>Account Type</label>
+	<select name="account_type">
+		<option value="checking">Checking</option>
+		<option value="savings">Savings</option>
+		<option value="loan">Loan</option>
+	</select>
+	<label>Balance</label>
+	<input type="number" min="0.00" name="balance" step="0.01"/>
+	<input type="submit" name="save" value="Create"/>
+</form>
