effb65fa3549cc21ea0c30ce6d33e44ed9809f49
NamanRaval <nyr2@njit.edu>
2021-11-26 17:09:15 -0500

    validation

--- public_html/Project/test_create_transactions.php
+++ public_html/Project/test_create_transactions.php
@@ -6,17 +6,14 @@ if (!has_role("Admin")) {
   die(header("Location: ../login.php"));
 }
 
-// init db
 $user = get_user_id();
 $db = getDB();
 
-// Get user accounts
 $stmt = $db->prepare('SELECT * FROM Accounts WHERE user_id = :id ORDER BY id ASC');
 $stmt->execute([':id' => $user]);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 if (isset($_POST["save"])) {
-  //TODO add proper validation/checks
   $account_src = $_POST["account_src"];
   $account_dest = $_POST["account_dest"];
   $transaction_type = $_POST["transaction_type"];
@@ -33,41 +30,3 @@ if (isset($_POST["save"])) {
   }
 }
 ?>
-
-<form method="POST">
-	<label>Account Src ID</label>
-<?php if (count($results) > 0): ?>
-	<select name="account_src">
-    <option value="1">000000000000 | world</option>
-  <?php foreach ($results as $r): ?>
-    <option value="<?php echo($r["id"]); ?>">
-      <?php echo($r["account_number"]); ?> | <?php echo($r["account_type"]); ?> | <?php echo($r["balance"]); ?>
-    </option>
-  <?php endforeach; ?>
-  </select>
-<?php endif; ?>
-	<label>Account Dest ID</label>
-<?php if (count($results) > 0): ?>
-	<select name="account_dest">
-    <option value="1">000000000000 | world</option>
-  <?php foreach ($results as $r): ?>
-    <option value="<?php echo($r["id"]); ?>">
-      <?php echo($r["account_number"]); ?> | <?php echo($r["account_type"]); ?> | <?php echo($r["balance"]); ?>
-    </option>
-  <?php endforeach; ?>
-  </select>
-<?php endif; ?>
-	<label>Transaction Type</label>
-	<select name="transaction_type">
-		<option value="deposit">Deposit</option>
-		<option value="withdraw">Withdraw</option>
-		<option value="transfer">Transfer</option>
-	</select>
-	<label>Balance Change</label>
-	<input type="number" min="0.00" name="balance" step="0.01"/>
-	<label>Memo</label>
-	<input type="text" name="memo"/>
-	<input type="submit" name="save" value="Create"/>
-</form>
-
-<?php require __DIR__ . "/../../partials/flash.php"; ?>
