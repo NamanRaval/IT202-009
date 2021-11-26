1da38905621a82c69191b2d6e8f74fd692fb1436
NamanRaval <nyr2@njit.edu>
2021-11-26 17:35:48 -0500

    Account viewing page

--- public_html/Project/test_view_accounts.php
+++ public_html/Project/test_view_accounts.php
@@ -21,3 +21,27 @@ if (isset($id)) {
   }
 }
 ?>
+
+<?php if (isset($result) && !empty($result)): ?>
+    <div class="card">
+        <div class="card-title">
+            <?php echo($result["id"]); ?>
+        </div>
+        <div class="card-body">
+            <div>
+                <p>Information</p>
+                <div>Account Number: <?php echo($result["account_number"]); ?></div>
+                <div>Account Type: <?php echo($result["account_type"]); ?></div>
+                <div>Balance: $<?php echo($result["balance"]); ?></div>
+                <div>Last Updated: <?php echo($result["last_updated"]); ?></div>
+                <div>Owned by: <?php echo($result["username"]); ?> (<?php echo($result["email"]); ?>)</div>
+                <div>Opened: <?php echo($result["opened_date"]); ?></div>
+            </div>
+        </div>
+    </div>
+<?php else: ?>
+    <p>Error looking up id...</p>
+<?php
+endif;
+require __DIR__ . "/../../partials/flash.php";
+?>
