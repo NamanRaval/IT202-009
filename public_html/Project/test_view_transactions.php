298dda1a3061a83e9729f844eb270acc2a299cfe
NamanRaval <nyr2@njit.edu>
2021-11-26 17:37:50 -0500

    transaction viewing page

--- public_html/Project/test_view_transactions.php
+++ public_html/Project/test_view_transactions.php
@@ -21,3 +21,29 @@ if (isset($id)) {
   }
 }
 ?>
+
+<?php if (isset($result) && !empty($result)): ?>
+    <div class="card">
+        <div class="card-title">
+            <?php echo($result["id"]); ?>
+        </div>
+        <div class="card-body">
+            <div>
+                <p>Information</p>
+                <div>Account Src: <?php echo($result["act_src_id"]); ?></div>
+                <div>Account Dest: <?php echo($result["act_dest_id"]); ?></div>
+                <div>Amount: $<?php echo($result["amount"]); ?></div>
+                <div>Type: <?php echo($result["action_type"]); ?></div>
+                <div>Memo: <?php echo($result["memo"]); ?></div>
+                <div>Expected Total: $<?php echo($result["expected_total"]); ?></div>
+                <div>Created: <?php echo($result["created"]); ?></div>
+            </div>
+        </div>
+    </div>
+<?php else: ?>
+    <p>Error looking up id...</p>
+<?php
+endif;
+require __DIR__ . "/../../partials/flash.php";
+?>
+
