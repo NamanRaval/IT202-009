3673f875d51adaf5d14240230f2a39728f130212
NamanRaval <nyr2@njit.edu>
2021-11-26 19:53:43 -0500

    fetching from table and validation for account redirect

---
+++ public_html/Project/accounts.php
@@ -0,0 +1,24 @@
+<?php
+require_once __DIR__ . "/partials/nav.php";
+if (!is_logged_in()) {
+  flash("You don't have permission to access this page");
+  die(header("Location: login.php"));
+}
+
+$results = [];
+if (isset($_SESSION['user']['username'])) {
+  $username = $_SESSION['user']['username'];
+}
+if (!empty($username)) {
+  $db = getDB();
+  $stmt = $db->prepare(
+    "SELECT account_number, account_type, balance FROM Accounts JOIN Users ON Accounts.user_id = Users.id WHERE Users.username = :q ORDER BY Accounts.id LIMIT 5"
+  );
+  $r = $stmt->execute([":q" => $username]);
+  if ($r) {
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  } else {
+    flash("There was a problem fetching the results");
+  }
+}
+?>
