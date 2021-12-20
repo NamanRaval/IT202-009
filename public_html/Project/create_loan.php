b199601510a03b1ab4f6324fdaeec7daafd68271
NamanRaval <nyr2@njit.edu>
2021-12-20 15:01:03 -0500

    formatting done

--- public_html/Project/create_loan.php
+++ public_html/Project/create_loan.php
@@ -47,3 +47,41 @@ if (isset($_POST["save"])) {
     }
     }
 ?>
+
+<h3 class="text-center mt-4">Open a Loan</h3>
+
+<form method="POST">
+  <div class="form-group">
+    <label for="deposit">Loan Principal</label>
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text">$</span>
+      </div>
+      <input type="number" class="form-control" id="deposit" min="500.00" name="balance" step="0.01" placeholder="500.00" aria-describedby="depositHelp"/>
+    </div>
+    <small id="depositHelp" class="form-text text-muted">Minimum $500 required.</small>
+  </div>
+  <div class="form-group">
+    <label for="account_dest">Deposit to Account</label>
+    <select class="form-control" id="account_dest" name="account_dest">
+      <?php foreach ($accounts as $r): ?>
+      <option value="<?php echo($r["id"]); ?>">
+        <?php echo($r["account_number"]); ?> | <?php echo($r["account_type"]); ?> | <?php echo($r["balance"]); ?>
+      </option>
+      <?php endforeach; ?>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="apy">APY</label>
+    <div class="input-group">
+      <div class="input-group-prepend">
+        <span class="input-group-text">%</span>
+      </div>
+      <input type="number" class="form-control" id="apy" min="2.00" name="apy" step="0.0001" placeholder="5.00" aria-describedby="depositHelp"/>
+    </div>
+    <small id="apyHelp" class="form-text text-muted">Minimum 2% APY.</small>
+  </div>
+  <button type="submit" name="save" value="create" class="btn btn-primary">Create</button>
+</form>
+
+<?php require __DIR__ . "/../../partials/flash.php"; ?>
