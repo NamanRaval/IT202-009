126d23e953e4f218c515ee8b956a6c959f2d8936
NamanRaval <nyr2@njit.edu>
2021-12-20 16:31:15 -0500

    validation

--- public_html/Project/close_account.php
+++ public_html/Project/close_account.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . "/../../partials/nav.php";
 if (!is_logged_in()) {
   flash("You don't have permission to access this page");
-  die(header("Location: login.php"));
+  redirect("login.php");
 }
 
 $user = get_user_id();
@@ -12,3 +12,27 @@ $stmt = $db->prepare("SELECT id, account_number, account_type, balance FROM Acco
 $stmt->execute([':id' => $user]);
 $accounts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+if (isset($_POST["save"])) {
+    $id = $_POST["account"];
+  
+    $stmt = $db->prepare('SELECT balance, account_number FROM Accounts WHERE id = :q');
+    $stmt->execute([ ":q" => $id ]);
+    $account = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($account["balance"] != 0) {
+      flash("Balance has to be $0 before closing.");
+      redirect("close_account.php");
+    }
+  
+    $user = get_user_id();
+    $stmt = $db->prepare(
+      "UPDATE Accounts SET active = 0 WHERE id = :id"
+    );
+    $r = $stmt->execute([ ":id" => $id ]);
+    if ($r) {
+      flash("Account ".$account["account_number"]." successfully closed.");
+      redirect("accounts.php");
+    } else {
+      flash("Error closing account!");
+    }
+  }
+?>
